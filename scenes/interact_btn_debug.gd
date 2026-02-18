c1b9f87de70e54fc5b1103d84f81f681886fd363
kawazzaki98 <nachreda02@gmail.com>
2026-02-18 05:55:41 -0800

    v0

--- scenes/interact_btn_debug.gd
+++
@@ -1,7 +0,0 @@
-extends interactOBJ_Debug
-
-
-
-func imgui_win():
-    ImGui.Begin(parent.name)
-    ImGui.Text("is button active : ")
--- scripts/player/jump.gd
+++
@@ -1,9 +0,0 @@
-extends State
-
-
-
-@export var jump_force : float
-
-
-func enter():
-	player_controller.velocity.y += jump_force
